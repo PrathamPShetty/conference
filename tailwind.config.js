24320c50f76438513971ddd3315eae4473bde1c0
Vrushali A Poojary <vrushaliash04@gmail.com>
2024-11-17 17:04:56 +0530

    background date time

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}", // Make sure this line is included to scan all HTML and TS files
+    "./src/**/*.{html,ts,jsx,tsx}", // Ensure all file types used in the project are scanned
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        // Customize breakpoints if needed
+        'xs': '475px', // Extra small devices
+      },
+    },
   },
   plugins: [],
-}
+};
